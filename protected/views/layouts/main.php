fb79c08c892ff2b53864c763192382f7c3e0f670
Indomian <blade39@yandex.ru>
2012-10-11 23:30:29 +0400

    Добавлен код метрики
    Signed-off-by: Indomian <blade39@yandex.ru>

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -51,6 +51,29 @@
 		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
+		<!-- Yandex.Metrika counter -->
+		<script type="text/javascript">
+			(function (d, w, c) {
+				(w[c] = w[c] || []).push(function() {
+					try {
+						w.yaCounter17627431 = new Ya.Metrika({id:17627431, enableAll: true});
+					} catch(e) { }
+				});
+
+				var n = d.getElementsByTagName("script")[0],
+						s = d.createElement("script"),
+						f = function () { n.parentNode.insertBefore(s, n); };
+				s.type = "text/javascript";
+				s.async = true;
+				s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+				if (w.opera == "[object Opera]") {
+					d.addEventListener("DOMContentLoaded", f);
+				} else { f(); }
+			})(document, window, "yandex_metrika_callbacks");
+		</script>
+		<noscript><div><img src="//mc.yandex.ru/watch/17627431" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+		<!-- /Yandex.Metrika counter -->
 	</div><!-- footer -->
 
 </div><!-- page -->
