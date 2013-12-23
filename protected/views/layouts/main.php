5206812924c1db8fc73e86d96ae70c72a91ab7ec
Indomian <blade39@yandex.ru>
2013-12-23 22:29:07 +0400

    Web player test
    Signed-off-by: Indomian <blade39@yandex.ru>

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -11,6 +11,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="/css/bootstrap-responsive.css" rel="stylesheet">
 		<link href="/css/style.css" rel="stylesheet">
+		<script type="text/javascript" src="/js/jwplayer/jwplayer.js"></script>
 	</head>
 	<body>
 		<div class="navbar navbar-static-top">
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -10,3 +10,35 @@ $this->pageTitle=Yii::app()->name;
 	свои финансовые операции.
 </p>
 <p>Спасибо <a href="http://wiki.enchtex.info">wiki.enchtex.info</a> за помощь в организации работы сайта.</p>
+
+<div>
+        <ul>
+                <li><a href="rtmp://itv08.digizuite.dk/tv2b" class="playerLink">Stream1</a></li>
+	        <li><a href="http://stream.flowplayer.org/bauhaus/624x260.mp4" class="playerLink">Stream2</a></li>
+	        <li><a href="/temp/1.mp3" class="playerLink">Stream3</a></li>
+        </ul>
+</div>
+<div id="player"></div>
+
+<script type="text/javascript">
+$(document).ready(function(){
+	jwplayer('player').setup({
+		width:600,
+		height:300,
+		file:"/temp/1.mp3",
+		primary:'flash',
+		onReady:function(){
+		},
+		onSetupError:function(fallback,message) {
+			alert(message);
+		}
+	});
+	$('a.playerLink').click(function(e){
+		e.preventDefault();
+		var url= $(this).attr('href');
+		jwplayer().stop();
+		jwplayer().load([{file:url}]);
+		jwplayer().play(true);
+	});
+});
+</script>
